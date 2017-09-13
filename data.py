f1ee04ab63f5a6fff03a6d1cd6d14f78e2feae0d
marcosantar93 <marcosantar93@gmail.com>
2017-09-13 16:20:48 -0300

    fin tuto 1

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'lorem',
+            'author':'Elver Galarga',
+            'create_date':'04.25.2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'lorem',
+            'author': 'Elver Gacorta',
+            'create_date': '04.25.2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'lorem',
+            'author': 'Elver Gamedia',
+            'create_date': '04.25.2017'
+        }
+
+    ]
+    return articles
